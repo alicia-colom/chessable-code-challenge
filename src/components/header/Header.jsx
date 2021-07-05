1c042f05eb7a595b8e5417b7d14a3d42e74fd8ae
alicia-colom <aliciacolom@gmail.com>
2021-07-05 22:53:35 +0200

    Fix styles in header using clamp(), for mobile first and responsive design

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,8 +5,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Header() {
   return (
-    <header className="headerContainer">
-      <h1 className="headerTitle">Chessable code challenge</h1>
+    <header>
+      <div className="preHeader"></div>
+      <div className="headerContainer">
+        <h1 className="headerTitle">Chessable code challenge</h1>
+      </div>
     </header>
   );
 }
