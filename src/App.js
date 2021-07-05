973c003d3db4ad96cbfa1282d770060a24a12caa
alicia-colom <aliciacolom@gmail.com>
2021-07-05 13:59:29 +0200

    Install npm packages (styled components, prettier, eslint plugin, react router dom) and Add EsLint and Prettier configuration files

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './App.css';
 
 function App() {
 	return (
-		<div className="App">
-			<header className="App-header">
-				<h1>Chessable code challenge</h1>
-			</header>
-		</div>
-	);
+    <div className="App">
+      <header className="App-header">
+        <h1>Chessable code challenge</h1>
+      </header>
+    </div>
+  );
 }
 
 export default App;
