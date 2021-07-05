8d4a83047dd50eda51846d35d36f8213cd7de88c
alicia-colom <aliciacolom@gmail.com>
2021-07-05 22:14:05 +0200

    Add styles to board grid to render light and dark tiles depending on its position

--- src/components/chessboard/Chessboard.jsx
+++ src/components/chessboard/Chessboard.jsx
@@ -1,18 +1,30 @@
 import React from "react";
 import "./Chessboard.css";
 import * as variables from "../../utils/constants";
+import styled from "styled-components";
 
 const Chessboard = () => {
+  const Tile = styled.div`
+    background-color: ${(props) =>
+      (props.x + props.y) % 2 === 0 ? "#cbdefc" : "#7296B8"};
+  `;
+
   const board = [];
   const xAxisReversed = variables.xAxis.reverse();
+
+  const isEven = (x, y) => {
+    (x + y) % 2 === 0 ? true : null;
+  };
 
   for (let iY = 0; iY < variables.yAxis.length; iY++) {
     for (let iX = 0; iX < xAxisReversed.length; iX++) {
       const eachTile = variables.yAxis[iY] + xAxisReversed[iX];
+      isEven(iX, iY);
+
       board.push(
-        <div key={eachTile} className="tile">
+        <Tile key={eachTile} className="tile" x={iX} y={iY}>
           {eachTile}
-        </div>
+        </Tile>
       );
     }
   }
